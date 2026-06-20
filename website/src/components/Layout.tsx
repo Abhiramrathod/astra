a3a1e807c4086f441bcf3c2bf93ce58ac8f15699
Abhir <abhir@astra.io>
2026-06-20 13:59:48 +0000

    Add page transitions, staggered card animations, hover effects

--- website/src/components/Layout.tsx
+++ website/src/components/Layout.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from 'react'
+import { useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const location = useLocation()
+
   return (
     <div className="layout">
       <Sidebar />
-      <main className="content">{children}</main>
+      <main className="content" key={location.pathname}>
+        <div className="page">{children}</div>
+      </main>
     </div>
   )
 }
